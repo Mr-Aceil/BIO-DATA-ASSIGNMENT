a81eaaeaff626b2a3b0da52bdceefa63a5246390
Mr-Aceil <mimiracle022@gmail.com>
2024-12-06 11:46:31 +0100

    Bio data first commit

---
+++ bio.js
@@ -0,0 +1,32 @@
+function displayid1(){
+    document.getElementById('i1')
+    .style.display= 'flex';
+
+    document.getElementById('i2')
+    .style.display= 'none';
+
+    document.getElementById('i3')
+    .style.display= 'none';
+}
+
+function displayid2(){
+    document.getElementById('i1')
+    .style.display= 'none';
+
+    document.getElementById('i2')
+    .style.display= 'flex';
+
+    document.getElementById('i3')
+    .style.display= 'none';
+}
+
+function displayid3(){
+    document.getElementById('i1')
+    .style.display= 'none';
+
+    document.getElementById('i2')
+    .style.display= 'none';
+
+    document.getElementById('i3')
+    .style.display= 'flex';
+}
